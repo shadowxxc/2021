e73bcef3e683ec92064aa3b8d69ece9f98b6ecea
chenxx <chenxx@bokecc.com>
2021-01-28 11:14:51 +0800

    init

---
+++ src/views/zrb/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+    aaaaaaaaaaaaaaaaa
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'insert',
+  data () {
+    return {
+      list:[],
+      current:{},
+    }
+  },
+
+}
+</script>
+
+<style >
+
+</style>
